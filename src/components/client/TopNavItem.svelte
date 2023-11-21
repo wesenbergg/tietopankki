07a56acb783b9d6b1cd05d1c7634af0ec0ec98e5
wesenbergg <jerjomkin.boriss@gmail.com>
2023-11-21 22:05:57 +0200

    Add styling components

---
+++ src/components/client/TopNavItem.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let label;
+  export let href;
+  export let current = false;
+</script>
+
+{#if current}
+  <li>
+    <a
+      href={`/${href}`}
+      class="block py-2 px-3 md:p-0 text-white bg-purple-600 rounded md:bg-transparent md:text-purple-700"
+      aria-current="page">{label}</a
+    >
+  </li>
+{:else}
+  <li>
+    <a
+      href={`/${href}`}
+      class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700"
+      >{label}</a
+    >
+  </li>
+{/if}
